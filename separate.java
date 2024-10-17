10dcc1ec188141238fa1dbaaeb87fa8af331fded
Stuti Sharma <103407150+stutisharma31@users.noreply.github.com>
2024-10-17 18:27:36 +0530

    Create separate.java

---
+++ separate.java
@@ -0,0 +1,14 @@
+#LEETCODE 2938. Separate Black and White Balls
+class Solution {
+    public long minimumSteps(String s) {
+        long swap = 0;
+        int black = 0; 
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '0') 
+                swap += (long) black; 
+            else
+                black++; 
+        }
+        return swap;
+    }
+}
