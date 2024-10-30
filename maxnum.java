3c927fa0e2667b5f991a9d90e008bfdcd9f520cd
Stuti Sharma <103407150+stutisharma31@users.noreply.github.com>
2024-10-30 19:02:48 +0530

    Update maxnum.java

--- maxnum.java
+++ maxnum.java
@@ -1,10 +1,27 @@
-def find_max(arr):
-    max_num = arr[0]
-    for num in arr:
-        if num > max_num:
-            max_num = num
-    return max_num
+// def find_max(arr):
+//     max_num = arr[0]
+//     for num in arr:
+//         if num > max_num:
+//             max_num = num
+//     return max_num
 
-# Example usage
-arr = [3, 5, 1, 9, 2]
-print(find_max(arr))  # Output: 9
+// # Example usage
+// arr = [3, 5, 1, 9, 2]
+// print(find_max(arr))  # Output: 9
+public class Main {
+    public static int findMax(int[] arr) {
+        int maxNum = arr[0];
+        for (int num : arr) {
+            if (num > maxNum) {
+                maxNum = num;
+            }
+        }
+        return maxNum;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3, 5, 1, 9, 2};
+        System.out.println("The maximum number is: " + findMax(arr));  // Output: 9
+    }
+}
+
