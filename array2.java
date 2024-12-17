2dbf1e927d4812fcf0ce9e69559c72dcc393588e
Stuti Sharma <103407150+stutisharma31@users.noreply.github.com>
2024-12-17 14:05:14 +0530

    Update array2.java

--- array2.java
+++ array2.java
@@ -1,1 +1,22 @@
+public class MaxSubarraySum {
+    public static int maxSubArraySum(int[] arr) {
+        int maxSum = Integer.MIN_VALUE, currentSum = 0;
+
+        for (int num : arr) {
+            currentSum += num;
+            if (currentSum > maxSum) {
+                maxSum = currentSum;
+            }
+            if (currentSum < 0) {
+                currentSum = 0;
+            }
+        }
+        return maxSum;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        System.out.println("Maximum Subarray Sum: " + maxSubArraySum(arr));
+    }
+}
 
