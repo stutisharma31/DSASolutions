8aa71bd30b2ae26703af3c80859d8ccda47a1289
Stuti Sharma <103407150+stutisharma31@users.noreply.github.com>
2024-12-17 14:05:32 +0530

    Update array3.java

--- array3.java
+++ array3.java
@@ -1,1 +1,25 @@
+public class MoveZeroes {
+    public static void moveZeroesToEnd(int[] arr) {
+        int index = 0; // Points to the position to place the non-zero element
+
+        for (int num : arr) {
+            if (num != 0) {
+                arr[index++] = num;
+            }
+        }
+
+        while (index < arr.length) {
+            arr[index++] = 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {0, 1, 0, 3, 12};
+        moveZeroesToEnd(arr);
+        System.out.print("Array after moving zeroes: ");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
 
