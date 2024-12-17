bd3383eb1d107db370caa055c6140348a5ccbfbb
Stuti Sharma <103407150+stutisharma31@users.noreply.github.com>
2024-12-17 14:04:58 +0530

    Update array1.java

--- array1.java
+++ array1.java
@@ -1,1 +1,21 @@
 
+public class SecondLargest {
+    public static int findSecondLargest(int[] arr) {
+        int largest = Integer.MIN_VALUE, secondLargest = Integer.MIN_VALUE;
+
+        for (int num : arr) {
+            if (num > largest) {
+                secondLargest = largest;
+                largest = num;
+            } else if (num > secondLargest && num != largest) {
+                secondLargest = num;
+            }
+        }
+        return (secondLargest == Integer.MIN_VALUE) ? -1 : secondLargest;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 4, 45, 99, 99};
+        System.out.println("Second Largest Element: " + findSecondLargest(arr));
+    }
+}
